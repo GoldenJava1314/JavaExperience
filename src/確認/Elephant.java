44e4a815df85d84f2e2dc93df5bae87d78d53ce2
GoldenJava1314 <just4269@gmail.com>
2025-08-15 08:44:21 +0800

    update

---
+++ src/確認/Elephant.java
@@ -0,0 +1,25 @@
+package 確認;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Elephant {
+	public static void main(String[] args) {
+		 List<String> fruits = List.of("banana","orange","apple","lemon");
+		    Stream<String> s1 = fruits.stream();
+		    Stream<String> s2 = s1.peek(i->System.out.print(i+" "));
+		    System.out.println("--------");
+		    Stream<String> s3 = s2.sorted();
+		    Stream<String> s4 = s3.peek(i->System.out.print(i+" "));
+		    System.out.println("--------");
+		    String strFruits = s4.collect(Collectors.joining(","));
+		    System.out.println();
+		    System.out.println(strFruits);
+	}
+	
+    }
