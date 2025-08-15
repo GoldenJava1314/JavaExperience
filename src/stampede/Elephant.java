822acfd4f5d7b379cc570dafa9f377ba2c72bb2a
GoldenJava1314 <just4269@gmail.com>
2025-08-15 08:40:27 +0800

    update

--- src/stampede/Elephant.java
+++ src/stampede/Elephant.java
@@ -6,25 +6,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Elephant {
 	public static void main(String[] args) {
-		List<String> fruits = List.of("apple", "banana", "kiwi", "orange");
-
-        // 串流管線：
-        // 1. 篩選出長度大於 4 的水果
-        // 2. 偷看每個經過的元素，並將其印出
-        // 3. 將所有元素轉換為大寫
-        // 4. 終端操作：收集成一個新的 List
-        List<String> result = fruits.stream()
-                .filter(fruit -> fruit.length() > 4)
-                .peek(fruit -> System.out.println("經過 filter 後的元素: " + fruit))
-                .map(String::toUpperCase)
-                .peek(fruit -> System.out.println("經過 map 後的元素: " + fruit))
-                .collect(Collectors.toList());
-
-        System.out.println("---");
-        System.out.println("最終結果: " + result);
+		 List<String> fruits = List.of("banana","orange","apple","lemon");
+		    Stream<String> s1 = fruits.stream();
+		    Stream<String> s2 = s1.peek(i->System.out.print(i+" "));
+		    System.out.println("--------");
+		    Stream<String> s3 = s2.sorted();
+		    Stream<String> s4 = s3.peek(i->System.out.print(i+" "));
+		    System.out.println("--------");
+		    String strFruits = s4.collect(Collectors.joining(","));
+		    System.out.println();
+		    System.out.println(strFruits);
 	}
 	
     }
